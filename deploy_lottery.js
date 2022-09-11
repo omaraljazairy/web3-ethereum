e72fe375d1dc918227312b9337e7fd7fbf380158
Omar Aljazairy <omar@fedal.nl>
2022-09-11 21:48:23 +0200

    tests and web completed

--- deploy_lottery.js
+++ deploy_lottery.js
@@ -16,8 +16,9 @@ const deploy = async () => {
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
-
-  console.log('Contract deployed to', result.options.address);
+  
+  console.log('Interface => ', interface)
+  console.log('Contract deployed to address => ', result.options.address);
   provider.engine.stop();
 };
 deploy();
